2b2fd0e190d2e6ec2095db54dc5df593c4ed33c1
meashishpatel <ashishpatel3009@gmail.com>
2024-12-23 12:27:59 +0530

    Wrap Dashboard component with ErrorBoundary in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import ErrorBoundary from "./components/ErrorBoundary";
 const App = () => {
   return (
     <div>
-      <h1>hell</h1>
+      <ErrorBoundary>
+        <Dashboard />
+      </ErrorBoundary>
     </div>
   );
 };
